a7a61ff0525696846438f6073d4ae151229a6bea
victhor <victorlopezcortes25@gmail.com>
2025-10-08 20:48:27 +0200

    Actually change to using new shaders TODO: Missing changing hlsl shaders so they do the same thing as the glsl ones

--- template_files/SDL3/shaders/shader.frag.glsl
+++ template_files/SDL3/shaders/shader.frag.glsl
@@ -1,8 +1,12 @@
-#version 450
+#version 460
 
-layout (location = 0) in vec4 vertColor;
-layout (location = 0) out vec4 color;
+layout(location = 0) in vec4 vertColor;
+layout(location = 1) in vec2 uv;
+
+layout(location = 0) out vec4 color;
+
+layout(set=2, binding=0) uniform sampler2D texSampler;
 
 void main() {
-    color = vertColor;
+    color = texture(texSampler, uv) * vertColor;
 }
--- template_files/SDL3/shaders/shader.vert.glsl
+++ template_files/SDL3/shaders/shader.vert.glsl
@@ -1,17 +1,20 @@
-#version 450
+#version 460
 
 layout (location = 0) in vec2 position;
+layout (location = 1) in vec2 uv;
 
 layout (location = 0) out vec4 outColor;
+layout (location = 1) out vec2 outUV;
 
 void main()
 {
     gl_Position = vec4(position, 0.0, 1.0);
-    if(gl_VertexIndex == 0) {
+    if((gl_VertexIndex % 3) == 0) {
         outColor = vec4(1.0, 0.0, 0.0, 1.0);
-    } else if(gl_VertexIndex == 1) {
+    } else if((gl_VertexIndex % 3) == 1) {
         outColor = vec4(0.0, 1.0, 0.0, 1.0);
-    } else if(gl_VertexIndex == 2) {
+    } else if((gl_VertexIndex % 3) == 2) {
         outColor = vec4(0.0, 0.0, 1.0, 1.0);
     }
+    outUV = uv;
 }
