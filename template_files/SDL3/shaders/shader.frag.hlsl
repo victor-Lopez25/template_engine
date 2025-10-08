92aa29cfcdd98b8bd462fdc9dce705f9e2669fc3
victhor <victorlopezcortes25@gmail.com>
2025-10-08 21:25:15 +0200

    HLSL shaders now work too!

--- template_files/SDL3/shaders/shader.frag.hlsl
+++ template_files/SDL3/shaders/shader.frag.hlsl
@@ -1,4 +1,13 @@
-float4 main(float4 color : TEXCOORD0) : SV_Target0
+struct Input
 {
-    return color;
+    float4 color : TEXCOORD0;
+    float2 uv : TEXCOORD1;
+};
+
+Texture2D<float4> texture : register(t0, space2);
+SamplerState smp : register(s0, space2);
+
+float4 main(Input input) : SV_Target0
+{
+    return texture.Sample(smp, input.uv) * input.color;
 }
--- template_files/SDL3/shaders/shader.vert.hlsl
+++ template_files/SDL3/shaders/shader.vert.hlsl
@@ -2,24 +2,27 @@ struct Input
 {
     uint vertexIdx : SV_VertexID;
     float2 position : TEXCOORD0;
+    float2 uv : TEXCOORD1;
 };
 
 struct Output
 {
-    float4 color : TEXCOORD0;
     float4 position : SV_Position;
+    float4 color : TEXCOORD0;
+    float2 uv : TEXCOORD1;
 };
 
 Output main(Input input)
 {
     Output output;
     output.position = float4(input.position, 0.0f, 1.0f);
-    if(input.vertexIdx == 0) {
+    if((input.vertexIdx % 3) == 0) {
         output.color = float4(1.0f, 0.0f, 0.0f, 1.0f);
-    } else if(input.vertexIdx == 1) {
+    } else if((input.vertexIdx % 3) == 1) {
         output.color = float4(0.0f, 1.0f, 0.0f, 1.0f);
-    } else if (input.vertexIdx == 2) {
+    } else if ((input.vertexIdx % 3) == 2) {
         output.color = float4(0.0f, 0.0f, 1.0f, 1.0f);
     }
+    output.uv = input.uv;
     return output;
 }
